11622a43db6bfa1f96f4ed517b1c8d6b6bd0191c
OnkarNanaware <154796622+OnkarNanaware@users.noreply.github.com>
2026-08-02 16:13:47 +0530

    Time: 0 ms (100%), Space: 10.5 MB (46.05%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
+        //thus alice have the advantages alice always wins she is smart 
+        //lee is dumb like you reading this solution 
      return true;   
     }
 };

78180de4d41d1fc8d164ebc4383ed1efea3416e6
OnkarNanaware <154796622+OnkarNanaware@users.noreply.github.com>
2026-09-09 22:04:01 +0530

    Time: 67 ms (7.53%), Space: 31.4 MB (60.69%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:int f(int n,int m,string& text1,string& text2,vector<vector<int>>&dp)
+{
+    if(n<0||m<0)
+      return 0;
+      if(dp[n][m]!=-1)
+      {
+        return dp[n][m];
+      }
+      if(text1[n]==text2[m])
+       {
+          return dp[n][m]=1+f(n-1,m-1,text1,text2,dp);
+       }
+       return dp[n][m]=0+max(f(n-1,m,text1,text2,dp),f(n,m-1,text1,text2,dp));
+}
+    int minInsertions(string s) {
+        int n=s.length();
+        string t=s;
+        reverse(s.begin(),s.end());
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        long long sub=f(n-1,n-1,s,t,dp);
+        return n-sub;   
+        
+    }
+};

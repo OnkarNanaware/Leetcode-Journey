9bfd036cfe1b4d498ef04821e2d20a54a82fd7e4
OnkarNanaware <154796622+OnkarNanaware@users.noreply.github.com>
2026-09-24 22:24:27 +0530

    Time: 470 ms (13.36%), Space: 223.9 MB (26.72%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:long f(int ind,int buy,int cap,vector<int>&p,int n,vector<vector<vector<int>>>&dp)
+ {  if(cap==0) return 0; // to track the maximum 2 transactions 
+    if(ind==n) return 0;
+    long profit=0;
+    if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+        if(buy)
+    { 
+        profit=max((-p[ind])+f(ind+1,0,cap,p,n,dp),0+f(ind+1,1,cap,p,n,dp));
+
+    }else
+    { 
+        profit=max(p[ind]+f(ind+1,1,cap-1,p,n,dp),0+f(ind+1,0,cap,p,n,dp));
+    }
+    return dp[ind][buy][cap]=profit;
+ }
+    int maxProfit(vector<int>& p) {
+        int n=p.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return f(0,1,2,p,n,dp);
+    }
+};
